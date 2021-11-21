23708090aba0cdfc9592a0ad6eda6533c6ed47e7
sandhi_artha <sandiartha99@yahoo.co.id>
2021-11-21 16:41:06 +0100

    fix hw instead of wh

--- image/proc.py
+++ image/proc.py
@@ -2,47 +2,47 @@ import numpy as np
 
 from .viz import show_stats
 
-def to_whc(image):
-    """swaps channel/band to last [width, height, channel]
+def to_hwc(image):
+    """swaps channel/band to last [height, width, channel]
     --------
-    image: np.array of [c,w,h] format
+    image: np.array of [c,h,w] format
     """
     return np.transpose(image,[1,2,0])
 
 
-def to_cwh(image):
-    """swaps channel/band to first [channel, width, height]
+def to_chw(image):
+    """swaps channel/band to first [channel, height, width]
     --------
-    image: np.array of [w,h,c] format
+    image: np.array of [h,w,c] format
     """
     return np.transpose(image,[2,0,1])
 
 def _norm_plane(plane):
     """normalization only on 1 plane
-    plane: np.array in format [w,h,c]
+    plane: np.array in format [h,w,c]
     """
-    plane =  plane - np.min(plane)
-    return plane / np.max(plane)
+    plane =  plane - np.nanmin(plane)
+    return plane / np.nanmax(plane)
 
 def normalize(plane, cfirst=False):
     """Scales pixel value to 0.0 - 1.0
     does not change image shape
     --------
-    plane: np.array in format [w,h,c]
-    cfirst: bool, use cfirst=1 if image format [c,w,h]
+    plane: np.array in format [h,w,c]
+    cfirst: bool, use cfirst=1 if image format [c,h,w]
     """
-    if cfirst: plane = to_whc(plane)
+    if cfirst: plane = to_hwc(plane)
 
     # 1ch image usually have only [w,h] or [w,h,1]
     if len(plane.shape) == 2 or plane.shape[-1] == 1:
         norm_image = _norm_plane(plane)
     else:
-        norm_image = np.zeros(plane.shape)
+        norm_image = np.zeros_like(plane, dtype=np.float32)
         for i in range(plane.shape[-1]):
             norm_image[:,:,i] = _norm_plane(plane[:,:,i])
 
     # swap back to original ch first
-    if cfirst: norm_image = to_cwh(norm_image)
+    if cfirst: norm_image = to_chw(norm_image)
 
     return norm_image
 
@@ -54,10 +54,10 @@ def standardize(plane, cfirst=False):
     """Scales pixel value to have mean=0.0 and std=1.0
     does not change image shape
     --------
-    plane: np.array in format [w,h,c]
-    cfirst: bool, use cfirst=1 if image format [c,w,h]
+    plane: np.array in format [h,w,c]
+    cfirst: bool, use cfirst=1 if image format [c,h,w]
     """
-    if cfirst: plane = to_whc(plane)
+    if cfirst: plane = to_hwc(plane)
 
     # 1ch image usually have only [w,h] or [w,h,1]
     if len(plane.shape) == 2 or plane.shape[-1] == 1:
@@ -68,7 +68,7 @@ def standardize(plane, cfirst=False):
             stan_image[:,:,i] = _stan_plane(plane[:,:,i])
 
     # swap back to original ch first
-    if cfirst: stan_image = to_cwh(stan_image)
+    if cfirst: stan_image = to_chw(stan_image)
 
     return stan_image
 
@@ -76,7 +76,7 @@ def test_lib():
     """test function
     """
     a1 = np.random.randint(0,10,[40,40,3])
-    a2 = to_cwh(a1)
+    a2 = to_chw(a1)
 
     # ch last
     show_stats(a1)
