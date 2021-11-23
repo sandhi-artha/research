b026af51da6ffeb71654fb243d950d75a8aa80fa
sandhi_artha <sandiartha99@yahoo.co.id>
2021-11-24 00:13:47 +0100

    add paddings for bounds

--- remote_sensing/tile_gen.py
+++ remote_sensing/tile_gen.py
@@ -11,16 +11,52 @@ def get_label_gdf(split, in_dir):
     fn = f'SN6_AOI_11_Rotterdam_Buildings_GT20sqm-{split.capitalize()}.geojson'
     return gpd.read_file(os.path.join(in_dir, fn))
 
-def raster_vector_tiling(cfg, labels, bounds, timestamp, in_path, out_path):
+def get_labels_bounds(label_dir):
+    """returns [labels, bounds]
+    each is dictionary containing 'train','val','test' split
+    labels : geodataframe of building footprints
+    bounds : shapely. boundary/extent where labels are available
+    """
+    labels = {
+        'train' : get_label_gdf('train', label_dir),
+        'val'   : get_label_gdf('val', label_dir),
+        'test'  : get_label_gdf('test', label_dir)
+    }
+
+    # give paddings to ensure same raster and vector coverage
+    # when using total_bounds, the train boundaries take square coverage
+    #   so raster at bot right corner has image but with labels cutoff
+    #   +- 100 for val is not enough (blame the tiling algo)
+    train_bounds = labels['train'].total_bounds
+    train_bounds[2] -= 400
+
+    val_bounds = labels['val'].total_bounds
+    val_bounds[0] += 400
+    val_bounds[2] -= 400
+
+    test_bounds = labels['test'].total_bounds
+    test_bounds[0] += 400
+    test_bounds[2] -= 400
+
+    bounds = {
+        'train' : box(*train_bounds),
+        'val'   : box(*val_bounds),
+        'test'  : box(*test_bounds)
+    }
+
+    return labels, bounds
+
+def raster_vector_tiling(cfg, labels, bounds, timestamp, orient, in_path, out_path):
     """creates
     """
     raster_dict = {}
     vector_dict = {}
     
     for split in ['train','val','test']:
-        fn = '{}_{}_{}_{}_s{}'.format(
+        fn = '{}_{}_o{}_{}_{}_s{}'.format(
             cfg["project"],
             timestamp,
+            orient,
             cfg["name"],
             split,
             cfg["stride"]
