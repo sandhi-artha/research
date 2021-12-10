81d3c4f11998195ecd42834907593df18142619e
sandhi_artha <sandiartha99@yahoo.co.id>
2021-12-10 03:05:09 +0100

    merged cfg with post_cfg

--- remote_sensing/dataset_cfg.py
+++ remote_sensing/dataset_cfg.py
@@ -14,20 +14,34 @@ cfg = {
     'sar_proc'      : 1,            # (int) num of process used, 1 proc needs up to 7GB RAM
     
     # tiling
-    'splits'        : ['train','val','test'],  # (list) what split to generate
+    'splits'        : ['train'],  # (list) what split to generate
     # if load_tile is 1, below data is where to load tiles from. Else, it's where to save data
     'project'       : 'sensor',
-    'name'          : 'base_test',
+    'name'          : 'base',       # (str) DO NOT USE 'train', 'val' or 'test' in the name
     'stride'        : 0,            # (int) 0, 80 unit in px
     'out_dir'       : '../../dataset/sensor', # '../../sensor',  # where the tiles will be stored
     'label_dir'     : '../../dataset/spacenet6-challenge/expanded/exp_geojson_buildings', # '../../expanded/geojson_buildings',  
     'tile_proc'     : 4,            # (int) num of process for tiling
-    'load_scheme'   : 1,            # (bool) 1 means load from tile_scheme folder instead of tiling from scratch, will only tile raster
+    'load_scheme'   : 0,            # (bool) 1 means load from tile_scheme folder instead of tiling from scratch, will only tile raster
     'verbose'       : 0,            # (int) 0 minimum logs, 1 for all info, 2 for necessary tiling
 }
 
-def check_path():
-    save_path = os.path.join(cfg["out_dir"], cfg["name"])
+post_cfg = {
+    # post-tiling - used to create tfrecord
+    # will read cfg from tile_dir instead of using cfg above
+    'tile_dir'      : '../../dataset/sensor/base/raster',  # where to load tiles and cfg for tfrec
+    'splits'        : ['train'],  # which split to create tfrecord
+    'tfrec_dir'     : 'tfrecords',   # folder to save tfrecords, change with post-tile versions
+    'tfrec_proc'    : 2,            # num of process used, 1 for serial
+    'load_fix'      : 0,            # 1 means load from vector_fix instead of vector
+    'channel'       : [1,4,3],
+    'out_precision' : 32,            # 8, 16 or 32
+    'tfrec_size'    : 7,          # num examples per tfrec
+    'rotation'      : 0,
+    'flip'          : 0,
+}
+
+def check_path(save_path):
     if os.path.isdir(save_path):
         user_in = input(f'directory to save: {save_path}, already exist and might contain data, proceed? (y/n)\n')
         if user_in == 'n':
